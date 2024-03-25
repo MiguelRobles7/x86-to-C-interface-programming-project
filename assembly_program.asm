64a1d47564fab718390079451b2e5441cc1a7454
MiguelRobles7 <donnaldmiguelrobles7@gmail.com>
2024-03-25 13:46:22 +0800

    setup base files

---
+++ assembly_program.asm
@@ -0,0 +1,18 @@
+; assembly part 
+
+section .data
+msg db "Hello World",13,10,0
+
+section .text
+bits 64
+default rel
+
+global asm_run
+extern printf
+
+asm_run:
+	sub rsp, 8*5
+	lea rcx, [msg]
+	call printf
+	add rsp, 8*5
+	ret
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+extern void asm_run();
+
+int main() {
+	asm_run();
+	return 0;
+}
