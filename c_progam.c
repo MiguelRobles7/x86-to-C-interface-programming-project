a9efdf551702e780fcf61cc34ca371c6ac111f71
MiguelRobles7 <donnaldmiguelrobles7@gmail.com>
2024-03-28 21:26:47 +0800

    rewrite c code to accept manual input

--- c_progam.c
+++ c_progam.c
@@ -1,37 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-// Perform the dot product of two vectors
-float dot_product(float* A, float* B, int n) {
-    float result = 0.0f;
+int c_run() {
+    int n;
+    float sdot = 0;
+    printf("Enter the length of the vectors: ");
+    scanf("%d", &n);
+
     for (int i = 0; i < n; i++) {
-        result += A[i] * B[i];
-    }
-    return result;
-}
+        float a = 0.0;
+        float b = 0.0;
+        printf("Value for A%d: ", i+1);
+        scanf("%f", &a);
 
-int main() {
-    int x;
-    scanf("%d", &x);
-    const int n = 1 << x;
-    float* A = (float*)malloc(n * sizeof(float));
-    float* B = (float*)malloc(n * sizeof(float));
+        printf("Value for B%d: ", i + 1);
+        scanf("%f", &b);
 
-    // Initialize vectors A and B with random values
-    srand(time(NULL));
-    for (int i = 0; i < n; i++) {
-        A[i] = (float)rand() / RAND_MAX;
-        B[i] = (float)rand() / RAND_MAX;
+        sdot += a * b;
     }
 
-    // Call the dot product function
-    float sdot = dot_product(A, B, n);
-
-    // Print the result
-    printf("Dot product: %.6f\n", sdot);
-
-    free(A);
-    free(B);
+    printf("Sdot: %f", sdot);
     return 0;
 }
