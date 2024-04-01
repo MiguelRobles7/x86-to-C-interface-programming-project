123b3d2fc3e143d4ab5a8d9fe586199c62197888
MiguelRobles7 <donnaldmiguelrobles7@gmail.com>
2024-04-01 15:14:16 +0800

    add comments

--- main.c
+++ main.c
@@ -6,22 +6,23 @@ extern float asm_run(float A, float B);
 extern float c_run(float A, float B);
 
 int main() {
-	int n = 0; // Vector Size
+	int n = 0; // Vector size
 
-	printf("Input Vector Size (n): ");
-	scanf_s("%d", &n);
-
-	float* A = (float*)malloc(n * sizeof(float));
-	float* B = (float*)malloc(n * sizeof(float));
-
-	int runs_needed = 30; // At least 30 runs
-	
 	double asm_time = 0;
 	double c_time = 0;
 
 	float asm_sdot = 0;
 	float c_sdot = 0;
 
+	int runs_needed = 30; // At least 30 runs
+
+	printf("Input Vector Size (n): ");
+	scanf_s("%d", &n);
+
+	// Initialize array sizes
+	float* A = (float*)malloc(n * sizeof(float));
+	float* B = (float*)malloc(n * sizeof(float));
+	
 	srand(time(NULL));
 
 	for (int i = 0; i < runs_needed; i++) {
@@ -31,7 +32,7 @@ int main() {
 			B[j] = (float)rand() / RAND_MAX;
 		}
 
-		// Measure Kernel Time
+		// Measure Assembly Time
 		clock_t begin = clock();
 		for (int k = 0; k < n; k++) {
 			asm_sdot += asm_run(A[k], B[k]);
@@ -53,17 +54,22 @@ int main() {
 		clock_t c_end = clock();
 		run_time = (double)(c_end - c_begin)/CLOCKS_PER_SEC;
 
+		// Display run results
 		printf("C Sdot: %f\n", c_sdot);
 		printf("C Time: %f\n", run_time);
 
 		c_time += run_time;
 
+		// Check if results are equal
 		if (c_sdot == asm_sdot) printf("Equal\n");
 		else printf("Not Equal\n");
 
+		// Reset values
 		asm_sdot = 0;
 		c_sdot = 0;
 	}
+
+	// Get average time
 	printf("\nAssembly Average Time: %f", asm_time / runs_needed);
 	printf("\nC Average Time: %f", c_time/runs_needed);
 	return 0;
